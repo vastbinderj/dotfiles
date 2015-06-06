6481e2b59e3abc69b9d975ba7b88fdacdcae8aba
James Vastbinder <james.vastbinder@gmail.com>
2015-06-06 16:39:13 -0700

    update symlink script

--- dotfiles/symlink-setup.sh
+++ dotfiles/symlink-setup.sh
@@ -3,13 +3,6 @@
 # this symlinks all the dotfiles (and .vim/) to ~/
 # it also symlinks ~/bin for easy updating
 
-
-# this is a messy edit of alrra's nice work here:
-#   https://raw.githubusercontent.com/alrra/dotfiles/master/os/create_symbolic_links.sh
-#   it should and needs to be improved to be less of a hack.
-
-
-
 # jump down to line ~140 for the start.
 
 
@@ -131,10 +124,6 @@ print_success() {
 }
 
 
-
-
-
-
 #
 # actual symlink stuff
 #
@@ -142,11 +131,8 @@ print_success() {
 
 # finds all .dotfiles in this folder
 declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
-FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim bin" # add in vim and the binaries
-
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 main() {
 
     local i=""
