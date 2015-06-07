c84820c6e1076863ecead8eb3adb5fc278b65a89
James Vastbinder <james.vastbinder@gmail.com>
2015-06-06 22:46:34 -0700

    support setup on ubuntu

--- dotfiles/setup-a-new-machine.sh
+++ dotfiles/setup-a-new-machine.sh
@@ -1,22 +1,51 @@
-# copy paste this file in bit by bit.
-# don't run it.
-  echo "do not run this script in one go. hit ctrl-c NOW"
-  read -n 1
-
-
 ##
 ## new machine setup.
 ##
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Found Mac OSX
 
-#
-# homebrew!
-#
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    #
+    # homebrew!
+    #
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# then install things
-./brew.sh
+    # then install things
+    ./brew.sh
 
+
+    # github.com/thebitguru/play-button-itunes-patch
+    # disable itunes opening on media keys
+    git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
+
+
+    # change to bash 4 (installed by homebrew)
+    BASHPATH=$(brew --prefix)/bin/bash
+    sudo echo $BASHPATH >> /etc/shells
+    chsh -s $BASHPATH # will set for current user only.
+    echo $BASH_VERSION # should be 4.x not the old 3.2.X
+    # Later, confirm iterm settings aren't conflicting.
+
+    # create GOPATH dirs
+    mkdir -p $HOME/go
+    mkdir -p $HOME/go/{src,pkg,bin}
+
+
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # Found Linux
+
+    if [ -f /etc/lsb-release  ]; then
+            # Found Ubuntu
+            . /etc/lsb-release
+            
+            # Install stuff
+            sudo apt-get update && sudo apt-get install bash-completion build-essential bzr ctags cmake golang grc python-dev rbenv ruby-build silversearcher-ag -y
+
+            # create GOPATH dirs
+            mkdir -p $HOME/go
+            mkdir -p $HOME/go/{src,pkg,bin}
+    fi
+fi
 
 # github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
@@ -30,21 +59,8 @@ chmod +x ~/code/z/z.sh
 # z hooked up in .bash_profile
 
 
-# github.com/thebitguru/play-button-itunes-patch
-# disable itunes opening on media keys
-git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
-
-
 # for the c alias (syntax highlighted cat)
 sudo easy_install Pygments
-
-
-# change to bash 4 (installed by homebrew)
-BASHPATH=$(brew --prefix)/bin/bash
-sudo echo $BASHPATH >> /etc/shells
-chsh -s $BASHPATH # will set for current user only.
-echo $BASH_VERSION # should be 4.x not the old 3.2.X
-# Later, confirm iterm settings aren't conflicting.
 
 
 # symlinks!
