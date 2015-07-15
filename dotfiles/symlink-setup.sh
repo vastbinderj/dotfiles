43c2920b312e76d368d13bec9ae7e4627f860737
James Vastbinder <james.vastbinder@gmail.com>
2015-07-14 17:47:32 -0700

    move login-shell to bin dir

--- dotfiles/symlink-setup.sh
+++ dotfiles/symlink-setup.sh
@@ -131,6 +131,8 @@ print_success() {
 
 # finds all .dotfiles in this folder
 declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
+# add in bin dir
+FILES_TO_SYMLINK="$FILES_TO_SYMLINK bin" # add in vim and the binaries
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 main() {
