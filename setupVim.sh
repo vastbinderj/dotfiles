1d606b972360cadfe2426de0d3f194d13fa84882
James Vastbinder <james.vastbinder@gmail.com>
2016-01-17 20:06:27 -0800

    restore support for neovim

--- setupVim.sh
+++ setupVim.sh
@@ -11,10 +11,15 @@ if [ ! -d "$HOME/.vim" ]; then
     mkdir -p $HOME/.vim
     mkdir -p $HOME/bundle
 fi
-
 # clone vundle in ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
+# set up compatibility with neovim
+if [ ! -d "$HOME/.config" ]; then
+    mkdir -p "${XDG_CONFIG_HOME:=$HOME/.config}"
+    ln -s ~/.vim "$XDG_CONFIG_HOME/nvim"
+    ln -s ~/.vimrc "$XDG_CONFIG_HOME/nvim/init.vim"
+fi
 
 # install vundle packages in vim
 vim +PluginInstall +qall
