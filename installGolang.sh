85fed052659d9876ed33bf29bcd214fd61d916d0
James Vastbinder <james.vastbinder@gmail.com>
2016-02-18 21:10:17 -0800

    upgrade to go1.6

--- installGolang.sh
+++ installGolang.sh
@@ -4,42 +4,42 @@
 # Install Golang from source and configure GOPATH
 ##
 
-WORKDIR=`pwd`
+WORKDIR=$(pwd)
 
 # first clean up soure dir if exists already
-if [ -d  $HOME/go ]; then 
-    rm -rf $HOME/go
+if [ -d  "$HOME/go" ]; then
+    rm -rf "$HOME/go"
 fi
 
 # install Go1.4.2 binaries
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    cd $HOME
+    cd "$HOME" || exit
     wget -qO- https://storage.googleapis.com/golang/go1.4.2.darwin-amd64-osx10.8.tar.gz | tar --transform 's/^go/go1.4/' -xvz
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     if [ $(uname -m) == 'x86_64' ]; then
-        cd $HOME
+        cd "$HOME" || exit
         wget -qO- https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz | tar --transform 's/^go/go1.4/' -xvz
     else
-        cd $HOME
+        cd "$HOME" || exit
         wget -qO- https://storage.googleapis.com/golang/go1.4.2.linux-386.tar.gz | tar --transform 's/^go/go1.4/' -xvz
     fi
 fi
 
 # create GOPATH dirt
 if [ ! -d "$HOME/code/go" ]; then
-    mkdir -p $HOME/code/go
-    mkdir -p $HOME/code/go/{src,pkg,bin}
+    mkdir -p "$HOME/code/go"
+    mkdir -p "$HOME/code/go/{src,pkg,bin}"
 fi
 
 
 # install golang from the source repository
-git clone https://github.com/golang/go $HOME/go
-cd $HOME/go
+git clone https://github.com/golang/go "$HOME/go"
+cd "$HOME/go" || exit
 
 
 # checkout go 1.5
-git checkout release-branch.go1.5
-cd $HOME/go/src
+git checkout release-branch.go1.6
+cd "$HOME/go/src" || exit
 
 
 # build from source
@@ -48,5 +48,5 @@ eval $CMD
 
 
 # back to working dir
-cd $WORKDIR
+cd "$WORKDIR" || exit
 unset CMD
