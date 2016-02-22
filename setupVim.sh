a8ea2b3b5c7aded266763acc9d48fdf90d241f81
James Vastbinder <james.vastbinder@gmail.com>
2016-02-22 07:42:58 +0000

    update nvim config

--- setupVim.sh
+++ setupVim.sh
@@ -16,7 +16,7 @@ git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 # set up compatibility with neovim
 if [ ! -d "$HOME/.config" ]; then
-    mkdir -p "${XDG_CONFIG_HOME:=$HOME/.config}"
+    mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
     ln -s ~/.vim "$XDG_CONFIG_HOME/nvim"
     ln -s ~/.vimrc "$XDG_CONFIG_HOME/nvim/init.vim"
 fi
