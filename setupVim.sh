2ffc2558cd5115295e9633a60128eb03f5730ad6
James Vastbinder <james.vastbinder@gmail.com>
2016-06-09 13:52:17 -0700

    move clone of color scheme

--- setupVim.sh
+++ setupVim.sh
@@ -11,6 +11,17 @@ if [ ! -d "$HOME/.vim" ]; then
     mkdir -p $HOME/.vim
     mkdir -p $HOME/bundle
 fi
+
+# set up material theme
+if [ -f $HOME/code/vim-hybrid-material  ]; then
+    mkdir -p $HOME/.vim/colors
+    cp $HOME/code/vim-hybrid-material/base16-material/base16-material-dark.vim $HOME/.vim/colors/
+else
+    git clone https://github.com/kristijanhusak/vim-hybrid-material $HOME/code/vim-hybrid-material
+    mkdir -p $HOME/.vim/colors
+    cp $HOME/code/vim-hybrid-material/base16-material/base16-material-dark.vim $HOME/.vim/colors/
+fi
+
 # clone vundle in ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
@@ -27,13 +38,4 @@ vim +PluginInstall +qall
 # install golang binaries
 vim +GoInstallBinaries +qall
 
-# set up material theme
-if [ -f $HOME/code/vim-hybrid-material  ]; then
-    mkdir -p $HOME/.vim/colors
-    cp $HOME/code/vim-hybrid-material/base16-material/base16-material-dark.vim $HOME/.vim/colors/
-else
-    git clone https://github.com/kristijanhusak/vim-hybrid-material $HOME/code/vim-hybrid-material
-    mkdir -p $HOME/.vim/colors
-    cp $HOME/code/vim-hybrid-material/base16-material/base16-material-dark.vim $HOME/.vim/colors/
-fi
 
