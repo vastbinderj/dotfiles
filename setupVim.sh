e956b67815f3d99cdace5dd9b8f3c9e2c4f1ddf8
James Vastbinder <james.vastbinder@gmail.com>
2016-06-10 22:46:16 -0700

    update vim setup script

--- setupVim.sh
+++ setupVim.sh
@@ -4,30 +4,28 @@
 #  Install Vim, Golang binaries and YCM
 ##
 
-WORKDIR=`pwd`
-
 # create .vim if it doesn't exist
 if [ ! -d "$HOME/.vim" ]; then
-    mkdir -p $HOME/.vim
-    mkdir -p $HOME/bundle
+    mkdir -p "$HOME/.vim"
+    mkdir -p "$HOME/bundle"
 fi
 
 # set up material theme
-if [ -f $HOME/code/vim-hybrid-material  ]; then
-    mkdir -p $HOME/.vim/colors
-    cp $HOME/code/vim-hybrid-material/base16-material/base16-material-dark.vim $HOME/.vim/colors/
+if [ -f "$HOME/code/vim-hybrid-material"  ]; then
+    mkdir -p "$HOME/.vim/colors"
+    cp "$HOME/code/vim-hybrid-material/base16-material/base16-material-dark.vim" "$HOME/.vim/colors/"
 else
-    git clone https://github.com/kristijanhusak/vim-hybrid-material $HOME/code/vim-hybrid-material
-    mkdir -p $HOME/.vim/colors
-    cp $HOME/code/vim-hybrid-material/base16-material/base16-material-dark.vim $HOME/.vim/colors/
+    git clone https://github.com/kristijanhusak/vim-hybrid-material "$HOME/code/vim-hybrid-material"
+    mkdir -p "$HOME/.vim/colors"
+    cp "$HOME/code/vim-hybrid-material/base16-material/base16-material-dark.vim" "$HOME/.vim/colors/"
 fi
 
 # clone vundle in ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 
 # set up compatibility with neovim
 if [ ! -d "$HOME/.config" ]; then
-    mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+    mkdir -p "${XDG_CONFIG_HOME:=$HOME/.config}"
     ln -s ~/.vim "$XDG_CONFIG_HOME/nvim"
     ln -s ~/.vimrc "$XDG_CONFIG_HOME/nvim/init.vim"
 fi
@@ -37,5 +35,4 @@ vim +PluginInstall +qall
 
 # install golang binaries
 vim +GoInstallBinaries +qall
-
 
